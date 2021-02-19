3c9e3ee7fb1b61221e58fa46e6219a1b001398e9
沈学海 <8425334@qq.com>
2021-02-19 21:52:07 +0800

    Initial commit

---
+++ src/views/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>后台管理系统</div>
+</template>
+
+<script>
+import GithubCorner from '@/components/GithubCorner'
+import PanelGroup from './dashboard/PanelGroup'
+import LineChart from './dashboard/LineChart'
+import RadarChart from '@/components/Echarts/RadarChart'
+import PieChart from '@/components/Echarts/PieChart'
+import BarChart from '@/components/Echarts/BarChart'
+
+
+export default {
+  name: 'Dashboard',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
